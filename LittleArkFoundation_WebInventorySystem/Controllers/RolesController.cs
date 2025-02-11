ebbe606c211094389aada4fd27190b4a39f6c94c
Cyril <Cyril@CYLAPTOP>
2025-02-11 17:57:09 +0800

    Added Roles Controller & Views

---
+++ LittleArkFoundation_WebInventorySystem/Controllers/RolesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LittleArkFoundation_WebInventorySystem.Controllers
+{
+    //TODO: Implement RolesModule
+    public class RolesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
